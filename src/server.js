452d3b2850288d0e7fc92788defaf7e036ed1845
tpmccallum <mistermac2008@gmail.com>
2020-04-27 15:14:40 +1000

    Allow up to 5 string or int arguments for RunInt and RunString

--- src/server.js
+++ src/server.js
@@ -65,139 +65,143 @@ raptor.method("ping", function(req) {
 
 // Load Wasm executable
 raptor.method("load_wasm_executable", function(req) {
-            console.log("Loading Wasm executable ... ");
-            console.log(req.params[0].wasm_description);
-            console.log(req.params[0].wasm_binary);
-            if (req.params[0].wasm_binary.startsWith("https://")) {
-                console.log("This a URL - we will go ahead and fetch this raw wasm file now ...");
-                urllib.request(req.params[0].wasm_binary, function(err, data, res) {
-                    if (err) {
-                        throw err; // you need to handle error
-                    }
-                    console.log(res.statusCode);
-                    console.log(res.headers);
-                    const hex = "0x" + data.toString('hex');
-                    console.log(hex);
-                    var sql = "INSERT INTO wasm_binary_files (wasm_description,wasm_binary) VALUES ('"+req.params[0].wasm_description+"','"+hex+"');";
-                    connection.query(sql, function(err, result) {
-                        if (err) {
-                            throw err;
-                        }
-                        console.log("1 record inserted");
-                        console.log(result.insertId);
-                    });
-                    
-                });
+    console.log("Loading Wasm executable ... ");
+    console.log(req.params[0].wasm_description);
+    console.log(req.params[0].wasm_binary);
+    if (req.params[0].wasm_binary.startsWith("https://")) {
+        console.log("This a URL - we will go ahead and fetch this raw wasm file now ...");
+        urllib.request(req.params[0].wasm_binary, function(err, data, res) {
+            if (err) {
+                throw err; // you need to handle error
             }
-            return result.insertId;
+            console.log(res.statusCode);
+            console.log(res.headers);
+            const hex = "0x" + data.toString('hex');
+            console.log(hex);
+            var sql = "INSERT INTO wasm_binary_files (wasm_description,wasm_binary) VALUES ('" + req.params[0].wasm_description + "','" + hex + "');";
+            connection.query(sql, function(err, result) {
+                if (err) {
+                    throw err;
+                }
+                console.log("1 record inserted");
+                console.log(result.insertId);
+            });
+
+        });
+    }
+    return result.insertId;
 })
 
-        // #TODO decide on the response object's design and then create and return it
+// #TODO decide on the response object's design and then create and return it
 
-        // Read Wasm executable
-        raptor.method("read_wasm_executable", function(req) {
-            console.log("Reading Wasm executable ... ");
-            console.log(req.params[0].wasm_id);
-            var sql = "SELECT wasm_binary from wasm_binary_files WHERE wasm_id = '" + req.params[0].wasm_id + "'";
-            connection.query(sql, function(err, result) {
-                if (err) throw err;
-                console.log("1 record retrieved");
-                console.log(result.toString());
-            });
-            // #TODO decide on the response object's design and then create and return it
-        })
+// Read Wasm executable
+raptor.method("read_wasm_executable", function(req) {
+    console.log("Reading Wasm executable ... ");
+    console.log(req.params[0].wasm_id);
+    var sql = "SELECT wasm_binary from wasm_binary_files WHERE wasm_id = '" + req.params[0].wasm_id + "'";
+    connection.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log("1 record retrieved");
+        console.log(result.toString());
+    });
+    // #TODO decide on the response object's design and then create and return it
+})
 
-        // Execute Wasm executable
-        raptor.method("execute_wasm_executable", function(req) {
-            console.log("Executing Wasm executable ... ");
-            // #TODO decide on the response object's design and then create and return it
-        })
+// Execute Wasm executable
+raptor.method("execute_wasm_executable", function(req) {
+    console.log("Executing Wasm executable ... ");
+    // #TODO decide on the response object's design and then create and return it
+})
 
-        // Update Wasm executable
-        raptor.method("update_wasm_executable", function(req) {
-            console.log("Updating Wasm executable ... ");
-            var sql = "UPDATE wasm_binary_files SET wasm_binary = '" + req.params[0].wasm_binary + "' WHERE wasm_id = '" + req.params[0].wasm_id + "'";
-            connection.query(sql, function(err, result) {
-                if (err) throw err;
-                console.log("1 record updated");
-                console.log(result);
-            });
-            // #TODO decide on the response object's design and then create and return it
-        })
+// Update Wasm executable
+raptor.method("update_wasm_executable", function(req) {
+    console.log("Updating Wasm executable ... ");
+    var sql = "UPDATE wasm_binary_files SET wasm_binary = '" + req.params[0].wasm_binary + "' WHERE wasm_id = '" + req.params[0].wasm_id + "'";
+    connection.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log("1 record updated");
+        console.log(result);
+    });
+    // #TODO decide on the response object's design and then create and return it
+})
 
-        // Remove Wasm executable
-        raptor.method("remove_wasm_executable", function(req) {
-            console.log("Removing Wasm executable ... ");
-            var sql = "DELETE from wasm_binary_files WHERE wasm_id = '" + req.params[0].wasm_id + "'";
-            connection.query(sql, function(err, result) {
-                if (err) throw err;
-                console.log("1 record deleted");
-                console.log(result);
-            });
-            // #TODO decide on the response object's design and then create and return it
-        })
+// Remove Wasm executable
+raptor.method("remove_wasm_executable", function(req) {
+    console.log("Removing Wasm executable ... ");
+    var sql = "DELETE from wasm_binary_files WHERE wasm_id = '" + req.params[0].wasm_id + "'";
+    connection.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log("1 record deleted");
+        console.log(result);
+    });
+    // #TODO decide on the response object's design and then create and return it
+})
 
-        // Temporary - execute Wasm executable from file system
-        // Takes request parameters like this (function_name as a string and arguments as a list)
-        // {"jsonrpc": "2.0", "method":"execute_via_file", "params":[{"function_name": "add", "arguments": [333, 555]}], "id": 1}
-        // {"jsonrpc": "2.0", "method":"execute_via_file", "params":[{"function_name": "say", "arguments": ["World"]}], "id": 1}
-        raptor.method("execute_hello_bg_file", function(req) {
-            console.log("Executing wasm ... ");
-            // This is temporary linking to wasm on file system. This will instantiate with Buffer (not file path) in the future
-            var vm = new ssvm.VM("/home/ubuntu/wasm_executables/hello_bg.wasm");
-            console.log(vm);
-            console.log(req.params[0].function_name);
-            console.log(req.params[0].arguments);
-            console.log("Total arguments provided = " + req.params[0].arguments.length);
-            var argument_list = req.params[0].arguments.toString();
-            if (typeof req.params[0].arguments[0] == "string") {
-                console.log("Calling run string: " + req.params[0].function_name + " with: " + argument_list);
-                switch (req.params[0].arguments.length){
-                    case 1:
+// Temporary - execute Wasm executable from file system
+// Takes request parameters like this (function_name as a string and arguments as a list)
+// {"jsonrpc": "2.0", "method":"execute_via_file", "params":[{"function_name": "add", "arguments": [333, 555]}], "id": 1}
+// {"jsonrpc": "2.0", "method":"execute_via_file", "params":[{"function_name": "say", "arguments": ["World"]}], "id": 1}
+raptor.method("execute_hello_bg_file", function(req) {
+    console.log("Executing wasm ... ");
+    // This is temporary linking to wasm on file system. This will instantiate with Buffer (not file path) in the future
+    var vm = new ssvm.VM("/home/ubuntu/wasm_executables/hello_bg.wasm");
+    console.log(vm);
+    console.log(req.params[0].function_name);
+    console.log(req.params[0].arguments);
+    console.log("Total arguments provided = " + req.params[0].arguments.length);
+    var argument_list = req.params[0].arguments.toString();
+    if (req.params[0].arguments.length <= 5) {
+        if (typeof req.params[0].arguments[0] == "string") {
+            console.log("Calling run string: " + req.params[0].function_name + " with: " + argument_list);
+            switch (req.params[0].arguments.length) {
+                case 1:
                     ret = vm.RunString(req.params[0].function_name, req.params[0].arguments[0]);
                     break;
-                    case 2:
+                case 2:
                     ret = vm.RunString(req.params[0].function_name, req.params[0].arguments[0], req.params[0].arguments[1]);
                     break;
-                    case 3:
+                case 3:
                     ret = vm.RunString(req.params[0].function_name, req.params[0].arguments[0], req.params[0].arguments[1], req.params[0].arguments[2]);
                     break;
-                    case 4:
+                case 4:
                     ret = vm.RunString(req.params[0].function_name, req.params[0].arguments[0], req.params[0].arguments[1], req.params[0].arguments[2], req.params[0].arguments[3]);
                     break;
-                    case 5:
+                case 5:
                     ret = vm.RunString(req.params[0].function_name, req.params[0].arguments[0], req.params[0].arguments[1], req.params[0].arguments[2], req.params[0].arguments[3], req.params[0].arguments[4]);
                     break;
-                }
-            } else if (typeof req.params[0].arguments[0] == "number") {
-                console.log("Calling run int: " + req.params[0].function_name + " with: " + argument_list);
-                switch (req.params[0].arguments.length){
-                    case 1:
+            }
+        } else if (typeof req.params[0].arguments[0] == "number") {
+            console.log("Calling run int: " + req.params[0].function_name + " with: " + argument_list);
+            switch (req.params[0].arguments.length) {
+                case 1:
                     ret = vm.RunInt(req.params[0].function_name, req.params[0].arguments[0]);
                     break;
-                    case 2:
+                case 2:
                     ret = vm.RunInt(req.params[0].function_name, req.params[0].arguments[0], req.params[0].arguments[1]);
                     break;
-                    case 3:
+                case 3:
                     ret = vm.RunInt(req.params[0].function_name, req.params[0].arguments[0], req.params[0].arguments[1], req.params[0].arguments[2]);
                     break;
-                    case 4:
+                case 4:
                     ret = vm.RunInt(req.params[0].function_name, req.params[0].arguments[0], req.params[0].arguments[1], req.params[0].arguments[2], req.params[0].arguments[3]);
                     break;
-                    case 5:
+                case 5:
                     ret = vm.RunInt(req.params[0].function_name, req.params[0].arguments[0], req.params[0].arguments[1], req.params[0].arguments[2], req.params[0].arguments[3], req.params[0].arguments[4]);
                     break;
-                }
             }
-            var response_object = {};
-            var key = "ssvm_response";
-            response_object[key] = [];
-            var data = ret;
-            response_object[key].push(data);
-            return response_object;
-        })
+        }
+    }
+    var response_object = {};
+    var key = "ssvm_response";
+    response_object[key] = [];
+    var data = ret;
+    response_object[key].push(data);
+    return response_object;
+})
 
-        // Serve
-        console.log("Starting server, please wait ... "); raptor.attach(server); server.listen(8081);
+// Serve
+console.log("Starting server, please wait ... ");
+raptor.attach(server);
+server.listen(8081);
 
-        // END
+// END
