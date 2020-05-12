c722a2a3ddc9cb51e85f9590410fc6aeb843b77a
tpmccallum <mistermac2008@gmail.com>
2020-05-12 15:38:13 +1000

    Resful API update

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
-
+require('dotenv').config()
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(process.api_port, process.env.host, () => console.log(`Welcome to wasm-joey`))
 
 /*
 
