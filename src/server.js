e8b481601642b657fe9ad899d82f3a4dc586d5ec
tpmccallum <mistermac2008@gmail.com>
2020-05-12 16:50:58 +1000

    Implement URL encoding and JSON

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 require('dotenv').config();
 
 app.get('/', (req, res) => res.send('Hello World!'))
