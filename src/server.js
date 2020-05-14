d6580a61a09b8de7d64875c3e8be83bba93a7adb
tpmccallum <mistermac2008@gmail.com>
2020-05-14 11:05:40 +1000

    Reduce query parser to simple operation to return array

--- src/server.js
+++ src/server.js
@@ -85,8 +85,8 @@ app.post('/api/executables', (req, res) => {
 app.get('/api/executables/:wasm_id', (req, res) => {
     var json_response = {};
     // filters include wasm_id, wasm_description, wasm_as_hex, wasm_as_buffer
-    console.log("TypeOf:" + typeof req.query.filterBy);
-    console.log(req.query.filterBy);
+    console.log("TypeOf:" + typeof req.query);
+    console.log(req.query);
     filters = querystring.parse(req.query.filterBy);
     console.log("TypeOf:" + typeof filters);
     console.log(filters);
