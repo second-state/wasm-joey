4cdb18a0719381d6b2ebeb69f2463f7dcd925aed
tpmccallum <mistermac2008@gmail.com>
2020-05-14 10:47:35 +1000

    Testing optional filtering via query string - type of params

--- src/server.js
+++ src/server.js
@@ -83,6 +83,7 @@ app.get('/api/executables/:wasm_id', (req, res) => {
     var json_response = {};
     // filters include wasm_id, wasm_description, wasm_as_hex, wasm_as_buffer
     filters = req.query.filterBy;
+    console.log("TypeOf:" + typeof filters);
     console.log(filters);
     if (filters.length >= 1) {
         if (filters.includes("wasm_as_hex")) {
