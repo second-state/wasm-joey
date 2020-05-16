4b98c348353c4b6fe7908c758dacfef602885866
tpmccallum <mistermac2008@gmail.com>
2020-05-16 11:49:47 +1000

    SSL port configuration

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const credentials = {
     ca: ca
 };
 //Port
-const port = process.env.port || 3000;
+const port = process.env.port;
 // Express
 const express = require('express');
 const app = express();
