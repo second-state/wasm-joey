5776bc8b89bcbe77a8df943814a9e0fc75ec56cf
tpmccallum <mistermac2008@gmail.com>
2020-05-21 15:47:00 +1000

    Trying req.body to mysql and back to JS as buffer

--- src/server.js
+++ src/server.js
@@ -94,7 +94,7 @@ app.post('/api/executables', (req, res) => {
     console.log("Request to set a new wasm hex into the database ...");
     if (req.is('text/plain') == 'text/plain') {
         console.log("Stringified body is: " + JSON.stringify(req.body));
-        var sqlInsert = "INSERT INTO wasm_executables (wasm_description,wasm_hex) VALUES ('" + req.header('SSVM-Description') + "','" + JSON.stringify(req.body) + "');";
+        var sqlInsert = "INSERT INTO wasm_executables (wasm_description,wasm_hex) VALUES ('" + req.header('SSVM-Description') + "','" + req.body + "');";
     } else if (req.is('application/octet-stream' == 'application/octet-stream')) {
         console.log("Wasm is in binary/asm format");
         // work out if we want to convert to hex here
@@ -163,7 +163,8 @@ app.get('/api/executables/:wasm_id', (req, res) => {
                         var sqlSelect = "SELECT wasm_hex from wasm_executables WHERE wasm_id = '" + req.params.wasm_id + "';";
                         console.log(sqlSelect);
                         performSqlQuery(sqlSelect).then((result) => {
-                            json_response["wasm_as_buffer"] = result[0].wasm_hex.toJSON();
+                            var new_buffer = Buffer.from(result[0].wasm_hex);
+                            json_response["wasm_as_buffer"] = new_buffer;
                             console.log(JSON.stringify("3" + JSON.stringify(json_response)));
                             if (filters.length == 0) {
                                 res.send(JSON.stringify(json_response));
