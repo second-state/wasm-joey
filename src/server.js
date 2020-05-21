55dc627574aed29e2bc2e606c8480dd039eec287
tpmccallum <mistermac2008@gmail.com>
2020-05-21 14:58:34 +1000

    Discover the object type of LONGTEXT that is returned to node.js

--- src/server.js
+++ src/server.js
@@ -192,15 +192,16 @@ app.get('/api/executables/:wasm_id', (req, res) => {
         performSqlQuery(sqlSelect).then((result) => {
             json_response["wasm_id"] = result[0].wasm_id;
             json_response["wasm_description"] = result[0].wasm_description;
-
+            json_response["wasm_as_hex"] = result2[0].wasm_hex;
+            console.log(typeof result2[0].wasm_hex);
         });
-        var sqlSelect2 = "SELECT wasm_hex from wasm_executables WHERE wasm_id = '" + req.params.wasm_id + "';";
+        /*var sqlSelect2 = "SELECT wasm_hex from wasm_executables WHERE wasm_id = '" + req.params.wasm_id + "';";
         performSqlQuery(sqlSelect2).then((result2) => {
             json_response["wasm_as_hex"] = result2[0].wasm_hex.toString('utf8');
             json_response["wasm_as_buffer"] = result2[0].wasm_hex.toJSON();
             res.send(JSON.stringify(json_response));
         });
-
+       */
     }
 
 });
