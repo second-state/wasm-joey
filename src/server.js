9de346066ac88b8b62eb1651a6dfebf3475e0d61
tpmccallum <mistermac2008@gmail.com>
2020-05-30 16:49:42 +1000

    Implementing callback

--- src/server.js
+++ src/server.js
@@ -355,9 +355,10 @@ app.post('/api/run/:wasm_id/:function_name', bodyParser.json(), (req, res) => {
                         var callback_object_for_processing = return_value_as_object["callback"];
                         console.log("*Callback Object: " + JSON.stringify(callback_object_for_processing));
                         delete return_value_as_object.callback;
+                        console.log("*Return value object: " + JSON.stringify(return_value_as_object));
                         //TODO strip out the callback object and pass exactly what is left of this response to the callback function as the --data payload
                         var new_return_value = executeCallback(callback_object_for_processing, return_value_as_object);
-                        console.log(new_return_value);
+                        console.log("*New value" + new_return_value);
                         json_response["return_value"] = new_return_value
                         console.log(json_response);
                         res.send(JSON.stringify(json_response));
