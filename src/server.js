a384b5f9464d54cfbeb3523974ae2fb84a3814e0
tpmccallum <mistermac2008@gmail.com>
2020-06-10 10:56:35 +1000

    Experiment with promise vs non

--- src/server.js
+++ src/server.js
@@ -517,7 +517,7 @@ app.post('/api/multipart/run/:wasm_id/:function_name', (req, res, next) => {
                             }
                             // The formidable file and fields iteration is performed separately by formidable middleware, this is a mechanism to let us know when the iterator has completed the task (avoid race conditions)
                             var readyAtZero = new ReadyAtZero(Object.keys(files).length + Object.keys(fields).length);
-                            parseMultipart(readyAtZero, files, fields, req) { // .then((m_result, m_error) => {
+                            parseMultipart(readyAtZero, files, fields, req);  // .then((m_result, m_error) => {
                                 if (!m_error) {
                                     while (true) {
                                         if (readyAtZero.isReady() == true) {
@@ -542,7 +542,7 @@ app.post('/api/multipart/run/:wasm_id/:function_name', (req, res, next) => {
                                 } else {
                                     console.log(m_error);
                                 }
-                            } //});
+                             //});
                         });
                     });
                 }
