7cd21f39dab7adb23617431825d13f19b0691ef4
tpmccallum <mistermac2008@gmail.com>
2020-07-06 13:39:33 +1000

    Wasm executable can not be deleted unless the caller uses the admin key

--- src/server.js
+++ src/server.js
@@ -685,11 +685,10 @@ app.delete('/api/executables/:wasm_id', (req, res) => {
                         joey_response["wasm_id"] = req.params.wasm_id
                         res.send(JSON.stringify(joey_response));
                     });
+                } else {
+                    joey_response["error"] = "Wrong admin key ... " + req.params.wasm_id + " can not be deleted.";
+                    res.send(JSON.stringify(joey_response));
                 }
-
-            } else {
-                joey_response["error"] = "Wrong admin key ... " + req.params.wasm_id + " can not be deleted.";
-                res.send(JSON.stringify(joey_response));
             });
         } else {
             joey_response["error"] = "wasm_id of " + req.params.wasm_id + " does not exist";
