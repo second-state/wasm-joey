c9f71dbc87c04929212fd6b9df4314770859707e
tpmccallum <mistermac2008@gmail.com>
2020-07-24 13:07:59 +1000

    Adding tests for getting executable and also using filters

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,9 @@
 // ************************************************************************************************
+// Joey production instance
+const joey_instance = "rpc.ssvm.secondstate.io";
+// Joey development instanance
+//const joey_instance = "dev.rpc.ssvm.secondstate.io";
+
 // Set up environment
 const https = require('https');
 var fs = require('fs');
@@ -6,6 +11,7 @@ var fs = require('fs');
 class WasmObject {
     constructor() {
         this.wasm_id;
+        this.wasm_description;
         this.SSVM_Admin_Key;
         this.SSVM_Usage_Key;
         this.wasm_sha256;
@@ -16,6 +22,12 @@ class WasmObject {
     }
     get_wasm_id() {
         return this.wasm_id;
+    }
+    set_wasm_description(_wasm_description) {
+        this.wasm_description = _wasm_description;
+    }
+    get_wasm_description() {
+        return this.wasm_description;
     }
     set_SSVM_Admin_Key(_SSVM_Admin_Key) {
         this.SSVM_Admin_Key = _SSVM_Admin_Key;
@@ -39,26 +51,29 @@ class WasmObject {
 }
 var wasm_object = new WasmObject();
 
+// ************************************************************************************************
+// Helper functions
 function printMessage(_message) {
     return new Promise(function(resolve, reject) {
-    if (_message.includes("error") || _message.includes("Error")){
-        console.log("\x1b[31m", _message);
-        resolve();
-    } else {
-    console.log("\x1b[32m", _message);
-    resolve();
-    }
-});
+        if (_message.includes("error") || _message.includes("Error")) {
+            console.log("\x1b[31m", _message);
+            resolve();
+        } else {
+            console.log("\x1b[32m", _message);
+            resolve();
+        }
+    });
 }
 
 // ************************************************************************************************
 // Load a new wasm executable
 function loadExecutable() {
+    console.log("\x1b[32m", "Processing: loadExecutable() ...");
     return new Promise(function(resolve, reject) {
         try {
             var options = {
                 'method': 'POST',
-                'hostname': 'rpc.ssvm.secondstate.io',
+                'hostname': joey_instance,
                 'port': 8081,
                 'path': '/api/executables',
                 'headers': {
@@ -99,19 +114,158 @@ function loadExecutable() {
         }
     });
 }
+
+// ************************************************************************************************
+// Get a wasm executable
+function getExecutable() {
+    console.log("Processing: getExecutable() ...");
+    var id_to_get = wasm_object.get_wasm_id();
+    return new Promise(function(resolve, reject) {
+        try {
+            var options = {
+                'method': 'GET',
+                'hostname': joey_instance,
+                'port': 8081,
+                'path': '/api/executables/' + id_to_get,
+                'headers': {},
+                'maxRedirects': 20
+            };
+            var req = https.request(options, function(res) {
+                var chunks = [];
+                res.on("data", function(chunk) {
+                    chunks.push(chunk);
+                });
+                res.on("end", function(chunk) {
+                    var body = Buffer.concat(chunks);
+                    var res_object = JSON.parse(body.toString());
+                    //console.log("Body:" + body.toString());
+                    //TODO read results and check
+                    if (wasm_object.get_wasm_id() == res_object["wasm_id"]) {
+                        printMessage("Success: " + wasm_object.get_wasm_id()).then((printResult) => {});
+                    } else {
+                        printMessage("Error, unable to obtain wasm_id in unit test called getExecutable()").then((printResult) => {});
+                    }
+                    resolve();
+                });
+                res.on("error", function(error) {
+                    printMessage(body.toString()).then((printResult) => {
+                        resolve();
+                    });
+                });
+            });
+            req.end();
+        } catch {
+            reject();
+        }
+    });
+}
+// ************************************************************************************************
+// Get a wasm executable
+function getExecutableFilterByDescription() {
+    console.log("Processing: getExecutableFilterByDescription() ...");
+    var id_to_get = wasm_object.get_wasm_id();
+    var query_string = '?filterBy=["wasm_description"]';
+    return new Promise(function(resolve, reject) {
+        try {
+            var options = {
+                'method': 'GET',
+                'hostname': joey_instance,
+                'port': 8081,
+                'path': '/api/executables/' + id_to_get + query_string,
+                'headers': {},
+                'maxRedirects': 20
+            };
+            var req = https.request(options, function(res) {
+                var chunks = [];
+                res.on("data", function(chunk) {
+                    chunks.push(chunk);
+                });
+                res.on("end", function(chunk) {
+                    var body = Buffer.concat(chunks);
+                    var res_object = JSON.parse(body.toString());
+                    //console.log("Body:" + body.toString());
+                    //TODO read results and check
+                    if (res_object["wasm_description"] == "Hello") {
+                        printMessage("Wasm description:: " + res_object["wasm_description"]).then((printResult) => {});
+                    } else {
+                        printMessage("Error, unable to obtain wasm_description in unit test called getExecutableFilterByDescription()").then((printResult) => {});
+                    }
+                    resolve();
+                });
+                res.on("error", function(error) {
+                    printMessage(body.toString()).then((printResult) => {
+                        resolve();
+                    });
+                });
+            });
+            req.end();
+        } catch {
+            reject();
+        }
+    });
+}
+
+// ************************************************************************************************
+// Get a wasm executable
+function getExecutableFilterBySha256() {
+    console.log("Processing: getExecutableFilterBySha256() ...");
+    var id_to_get = wasm_object.get_wasm_id();
+    var query_string = '?filterBy=["wasm_sha256"]';
+    return new Promise(function(resolve, reject) {
+        try {
+            var options = {
+                'method': 'GET',
+                'hostname': joey_instance,
+                'port': 8081,
+                'path': '/api/executables/' + id_to_get + query_string,
+                'headers': {},
+                'maxRedirects': 20
+            };
+            var req = https.request(options, function(res) {
+                var chunks = [];
+                res.on("data", function(chunk) {
+                    chunks.push(chunk);
+                });
+                res.on("end", function(chunk) {
+                    var body = Buffer.concat(chunks);
+                    var res_object = JSON.parse(body.toString());
+                    //console.log("Body:" + body.toString());
+                    //TODO read results and check
+                    if (res_object["wasm_sha256"] == wasm_object.get_wasm_sha256()) {
+                        printMessage("Wasm sha256:: " + res_object["wasm_sha256"]).then((printResult) => {});
+                    } else {
+                        printMessage("Error, unable to obtain wasm_sha256 in unit test called getExecutableFilterBySha256()").then((printResult) => {});
+                    }
+                    resolve();
+                });
+                res.on("error", function(error) {
+                    printMessage(body.toString()).then((printResult) => {
+                        resolve();
+                    });
+                });
+            });
+            req.end();
+        } catch {
+            reject();
+        }
+    });
+}
 // ************************************************************************************************
 // Delete the wasm executable (clean up after tests)
 function deleteExecutable() {
+    console.log("Processing: deleteExecutable() ...");
     return new Promise(function(resolve, reject) {
         try {
             var path_to_delete_wasm = "/api/executables/" + wasm_object.get_wasm_id();
             var admin_key_required_for_deletion = wasm_object.get_SSVM_Admin_Key();
             var options = {
                 'method': 'DELETE',
-                'hostname': 'rpc.ssvm.secondstate.io',
+                'hostname': joey_instance,
                 'port': 8081,
                 'path': path_to_delete_wasm,
-                'headers': { 'SSVM_Admin_Key': admin_key_required_for_deletion},
+                'headers': {
+                    'SSVM_Admin_Key': admin_key_required_for_deletion
+                },
                 'maxRedirects': 20
             };
             var req = https.request(options, function(res) {
@@ -140,6 +294,17 @@ function deleteExecutable() {
 // ************************************************************************************************
 // Execute the tests
 loadExecutable().then((loadExecutableResult) => {
-    deleteExecutable().then((deleteExecutableResult) => {
+    getExecutable().then((getExecutableResult) => {
+        getExecutableFilterByDescription().then((ggetExecutableFilterByDescriptionResult) => {
+            getExecutableFilterBySha256().then((getExecutableFilterBySha256Result) => {
+                deleteExecutable().then((deleteExecutableResult) => {
+
+                });
+            });
+        });
     });
 });
+
+// Checks for writing tests
+// Are headers correct in the request?
+// Are the correct REST verbs being used i.e GET vs POST
