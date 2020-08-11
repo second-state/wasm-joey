555bdf355dce75b721866f42ea49451f990b055d
tpmccallum <mistermac2008@gmail.com>
2020-08-11 12:22:15 +1000

    Add default env and preopens values to wasi object and fetch state from DB also

--- src/server.js
+++ src/server.js
@@ -387,16 +387,18 @@ function parseMultipart(_readyAtZero, _files, _fields, _req) {
 }
 
 function executeSSVM(_readyAtZero, _wasm_id, _function_name, _array_of_parameters, _return_type) {
-    var wasi = {
-        "args": [],
-        "env": {},
-        "preopens": {}
-    };
     var _joey_response = {};
     return new Promise(function(resolve, reject) {
         var sqlSelect = "SELECT wasm_binary, wasm_state from wasm_executables WHERE wasm_id = '" + _wasm_id + "';";
         performSqlQuery(sqlSelect).then((result2, error2) => {
             var uint8array = new Uint8Array(result2[0].wasm_binary.toString().split(','));
+            var wasm_state = result2[0].wasm_state;
+            var wasi = {
+                "args": [],
+                "env": {"wasm_id": _wasm_id},
+                "preopens": {"/": "/tmp"}
+            };
+            wasi.args[0] = wasm_state;
             var vm = new ssvm.VM(uint8array, wasi);
             if (_readyAtZero.fetchable_already_set == true) {
                 var fetchable_object = _readyAtZero.get_fetchable_object();
