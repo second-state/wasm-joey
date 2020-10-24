4dcf93c4f67b15c979084ba56b775ad5a8ce0ee3
tpmccallum <mistermac2008@gmail.com>
2020-10-24 11:17:54 +1000

    Adding comments to track AOT filenames

--- src/server.js
+++ src/server.js
@@ -542,7 +542,9 @@ function executeSSVM(_readyAtZero, _wasm_id, _storage_key, _function_name, _arra
             performSqlQuery(sqlSelect).then((result2, error2) => {
                 var wasm_state = result2[0].wasm_state;
                 ssvm_options.args[0] = wasm_state;
+                console.log("Instantiating SSVM with AOT filename of: " + aot_filename + "which has a typeof: " + typeof(aot_filename));
                 var vm = new ssvm.VM(aot_filename, ssvm_options);
+                console.log("Instantiation success!");
                 if (_readyAtZero.fetchable_already_set == true) {
                     var fetchable_object = _readyAtZero.get_fetchable_object();
                     if (fetchable_object.hasOwnProperty("GET")) {
