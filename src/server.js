3b1fb4ad93fc34d34af91aba6b818e55cf2954cc
tpmccallum <mistermac2008@gmail.com>
2020-12-04 18:08:15 +1000

    Return usage object

--- src/server.js
+++ src/server.js
@@ -184,7 +184,7 @@ function readLines(_readInterface2) {
             var split_time_gas = line.split(",");
             internal_object[split_time_gas[0]] = split_time_gas[1];
         });
-        console.log("Internal object: " + internal_object);
+        console.log("Internal object: " + JSON.stringify(internal_object));
         resolve(internal_object);
     });
 }
