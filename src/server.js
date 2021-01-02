dad0f83c3bdcf588d0b4c83220310d5d8dc9d632
tpmccallum <mistermac2008@gmail.com>
2021-01-02 10:15:49 +1000

    Bumping default rate limit (from 10) to 100 requests (per IP) per minute

--- src/server.js
+++ src/server.js
@@ -127,7 +127,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minute
-    max: 10 // limit each IP to 2 requests per windowMs
+    max: 100 // limit each IP to 2 requests per windowMs
 });
 
 //  apply to all requests
